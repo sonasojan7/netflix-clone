99f6ed78874d828b78f15521d5f8d019ea7292f5
sonasojan7 <sonasojan9656@gmail.com>
2025-06-08 21:31:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
